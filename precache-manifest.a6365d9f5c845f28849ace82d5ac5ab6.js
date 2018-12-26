99e5b638eca684524348c3662433d4edf65c0d08
tyenss <yuuki.butler@gmail.com>
2018-12-25 21:03:51 -0500

    Updates

---
+++ precache-manifest.a6365d9f5c845f28849ace82d5ac5ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e563d2985a662b59d2ee55f49fc6612b",
+    "url": "/academicreader/static/media/uss-george-washington-aircraft-carrier.e563d298.jpg"
+  },
+  {
+    "revision": "70a84698fc4b664fd19b",
+    "url": "/academicreader/static/js/runtime~main.70a84698.js"
+  },
+  {
+    "revision": "ed27c6b52e8adc02046d",
+    "url": "/academicreader/static/js/main.ed27c6b5.chunk.js"
+  },
+  {
+    "revision": "e7e9c45feb1d3c12ec83",
+    "url": "/academicreader/static/js/1.e7e9c45f.chunk.js"
+  },
+  {
+    "revision": "ed27c6b52e8adc02046d",
+    "url": "/academicreader/static/css/main.f1f08a1e.chunk.css"
+  },
+  {
+    "revision": "4239009814d5f336b212b8c4f4196619",
+    "url": "/academicreader/index.html"
+  }
+];
